69ebc1e9803a0e547d95a238b4cfa6f90356ed53
Paul Ron La <paul.la@utexas.edu>
2021-07-27 21:56:15 -0500

    initialized framework

---
+++ src/main/java/com/github/paulronla/spring/teochewdictionarybackend/TeochewDictionaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.paulronla.spring.teochewdictionarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeochewDictionaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeochewDictionaryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/paulronla/spring/teochewdictionarybackend/TeochewDictionaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paulronla.spring.teochewdictionarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeochewDictionaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
